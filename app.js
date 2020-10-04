564004f4183de71f932e9c5c44d3001a09172ba5
Mark Leo Tarectecan <ratrateroo@gmail.com>
2020-10-04 23:40:21 +0800

    added some needed dependencies

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql');
 
 const app = express();
 
 app.use(bodyParser.json());
+
+app.use('/graphql')
 
 app.listen(3000);
