ca8aafd21d6025fa080ea23b567224bef10ea618
Mark Leo Tarectecan <ratrateroo@gmail.com>
2020-10-06 14:50:59 +0800

    added event type and input type for event argument

--- app.js
+++ app.js
@@ -11,12 +11,28 @@ app.use(
   "/graphql",
   graphqlHTTP({
     schema: buildSchema(`
+        type Event {
+          _id: ID!
+          title: String!
+          description: String!
+          price: Float!
+          date: String!
+        }
+
+        input EventInput {
+          title: String!
+          description: String
+          price: Float!
+          date: String!
+        }
+
+
         type RootQuery {
-            events: [String!]!
+            events: [Event!]!
         }
 
         type RootMutation {
-            createEvent(name: String): String
+            createEvent(name: String): Event
         }
 
         schema {
