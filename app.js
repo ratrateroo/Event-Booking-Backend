9ca710725549ee92959a8d356cc6a3c627897ed4
Mark Leo Tarectecan <ratrateroo@gmail.com>
2020-10-07 23:42:55 +0800

    added mongoose for mongodb

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const { graphqlHTTP } = require("express-graphql");
 const { buildSchema } = require("graphql");
+const mongoose = require("mongoose");
 
 const app = express();
 
