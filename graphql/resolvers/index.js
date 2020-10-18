4c47ddb1ecafc4f0976fcbe3381d27ffc247edb7
Mark Leo Tarectecan <ratrateroo@gmail.com>
2020-10-18 19:58:19 +0800

    added findone for user to prevent duplicate user

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -70,8 +70,13 @@ module.exports = {
       },
 
       createUser: (args) => {
-        return bcrypt
-          .hash(args.userInput.password, 12)
+        return User.findOne({ email: args.userInput.email})
+          .then(user => {
+            if (user) {
+              throw new Error('User exists already.');
+            }
+            return bcrypt.hash(args.userInput.password, 12);
+          })        
           .then((hashedPassword) => {
             const user = new User({
               email: args.userInput.email,
