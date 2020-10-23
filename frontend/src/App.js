d98a531b637a88eae9dfe6620c222d2a39108bb3
Mark Leo Tarectecan <ratrateroo@gmail.com>
2020-10-23 19:47:19 +0800

    added pages for the front end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
+
+import AuthPage from './pages/Auth';
+import EventsPage from './pages/Events';
+import BookingsPage from './pages/Bookings';
 
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>It Works.</h1>
-    </div>
-  );
+class App extends Component {
+
+  render() {
+    return (
+      <Router>
+        <Switch>
+        <Redirect path="/" to="/auth" exact/>
+        <Route path="/auth" component={AuthPage} />
+        <Route path="/events" component={EventsPage} />
+        <Route path="/bookings" component={BookingsPage} />
+        </Switch>
+      </Router>
+    )
+  }
+
 }
+
+
 
 export default App;
