5a3c850c700ddebd18f543c9a04fdd4f3c6f3083
Mark Leo Tarectecan <ratrateroo@gmail.com>
2020-10-24 20:14:48 +0800

    added some styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,36 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from 'react-router-dom';
 
 import AuthPage from './pages/Auth';
 import EventsPage from './pages/Events';
 import BookingsPage from './pages/Bookings';
+import MainNavigation from './components/Navigation/MainNavigation';
 
 import './App.css';
 
 class App extends Component {
-
   render() {
     return (
       <Router>
-        <Switch>
-        <Redirect path="/" to="/auth" exact/>
-        <Route path="/auth" component={AuthPage} />
-        <Route path="/events" component={EventsPage} />
-        <Route path="/bookings" component={BookingsPage} />
-        </Switch>
+        <React.Fragment>
+          <MainNavigation />
+          <main className="main-content">
+            <Switch>
+              <Redirect path="/" to="/auth" exact />
+              <Route path="/auth" component={AuthPage} />
+              <Route path="/events" component={EventsPage} />
+              <Route path="/bookings" component={BookingsPage} />
+            </Switch>
+          </main>
+        </React.Fragment>
       </Router>
-    )
+    );
   }
-
 }
-
-
 
 export default App;
