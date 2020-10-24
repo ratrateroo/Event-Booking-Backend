2dfa3d26d6f1d13d24a19d2a0834f77ad75e6397
Mark Leo Tarectecan <ratrateroo@gmail.com>
2020-10-24 23:15:11 +0800

    allowed CORS

--- app.js
+++ app.js
@@ -1,22 +1,29 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const { graphqlHTTP } = require("express-graphql");
-const mongoose = require("mongoose");
+const express = require('express');
+const bodyParser = require('body-parser');
+const { graphqlHTTP } = require('express-graphql');
+const mongoose = require('mongoose');
 
 const graphQlSchema = require('./graphql/schema/index');
 const graphQlResolvers = require('./graphql/resolvers/index');
 const isAuth = require('./middleware/is-auth');
 
-
 const app = express();
 
-
 app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+});
 
 app.use(isAuth);
 
 app.use(
-  "/graphql",
+  '/graphql',
   graphqlHTTP({
     schema: graphQlSchema,
     rootValue: graphQlResolvers,
@@ -29,8 +36,8 @@ const url = `mongodb://127.0.0.1:27017/${process.env.MONGO_DB}`;
 mongoose
   .connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
-    console.log("Connected to the Database.");
-    app.listen(3000);
+    console.log('Connected to the Database.');
+    app.listen(8000);
   })
   .catch((error) => {
     console.log(error);
--- frontend/src/pages/Auth.js
+++ frontend/src/pages/Auth.js
@@ -1,12 +1,65 @@
 import React, { Component } from 'react';
 
-class AuthPage extends Component {
-    render() {
-        return (
-            <h1>The Auth Page</h1>
-        );
+import './Auth.css';
 
+class AuthPage extends Component {
+  constructor(props) {
+    super(props);
+    this.emailEl = React.createRef();
+    this.passwordEl = React.createRef();
+  }
+
+  submitHander = (event) => {
+    event.preventDefault();
+    const email = this.emailEl.current.value;
+    const password = this.passwordEl.current.value;
+
+    if (email.trim().length === 0 || password.trim().length === 0) {
+      return;
     }
+
+    console.log(email, password);
+
+    const requestBody = {
+      query: `
+            mutation {
+                createUser(userInput: {email: "${email}", password:"${password}"}) {
+                    _id
+                    email
+                }
+            }
+        `,
+    };
+
+    fetch('http://localhost:8000/graphql', {
+      method: 'POST',
+      body: JSON.stringify(requestBody),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  };
+
+  render() {
+    return (
+      <form className="auth-form" onSubmit={this.submitHander}>
+        <div className="form-control">
+          <label htmlFor="email">E-Mail</label>
+          <input type="email" id="email" ref={this.emailEl} />
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" ref={this.passwordEl} />
+        </div>
+
+        <div className="form-actions">
+          <button type="submit">Submit</button>
+          <button type="button">Swith to Signup</button>
+        </div>
+      </form>
+    );
+  }
 }
 
 export default AuthPage;
