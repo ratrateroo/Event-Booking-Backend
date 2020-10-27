068d948a9b4a08fecaa953be8b262dd2f5ee6c25
Mark Leo Tarectecan <ratrateroo@gmail.com>
2020-10-28 00:08:46 +0800

    added form to the modal for data gathering

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -9,12 +9,35 @@ class EventsPage extends Component {
     creating: false,
   };
 
+  constructor(props) {
+    super(props);
+    this.titleElRef = React.createRef();
+    this.priceElRef = React.createRef();
+    this.dateElRef = React.createRef();
+    this.descriptionElRef = React.createRef();
+  }
+
   startCreateEventHandler = () => {
     this.setState({ creating: true });
   };
 
   modalConfirmHandler = () => {
     this.setState({ creating: false });
+    const title = this.titleElRef.current.value;
+    const price = this.priceElRef.current.value;
+    const date = this.dateElRef.current.value;
+    const description = this.descriptionElRef.current.value;
+
+    if (
+      title.trim().length === 0 ||
+      price.trim().length === 0 ||
+      date.trim().length === 0 ||
+      description.trim().length === 0
+    ) {
+      return;
+    }
+    const event = { title, price, date, description };
+    console.log(event);
   };
 
   modalCancelHandler = () => {
@@ -33,7 +56,30 @@ class EventsPage extends Component {
             onCancel={this.modalCancelHandler}
             onConfirm={this.modalConfirmHandler}
           >
-            <p>Modal Content</p>
+            <form>
+              <div className="form-control">
+                <label htmlFor="title">Title</label>
+                <input type="text" id="title" ref={this.titleElRef} />
+              </div>
+
+              <div className="form-control">
+                <label htmlFor="price">Price</label>
+                <input type="number" id="price" ref={this.priceElRef} />
+              </div>
+
+              <div className="form-control">
+                <label htmlFor="title">Date</label>
+                <input type="date" id="date" ref={this.dateElRef} />
+              </div>
+              <div className="form-control">
+                <label htmlFor="description">Description</label>
+                <textarea
+                  id="description"
+                  rows="4"
+                  ref={this.descriptionElRef}
+                />
+              </div>
+            </form>
           </Modal>
         )}
         <div className="events-control">
