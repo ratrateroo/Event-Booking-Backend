c636113e25f2057a1b1c3c7480f912d8ea3ee542
Mark Leo Tarectecan <ratrateroo@gmail.com>
2020-11-04 23:19:55 +0800

    added more dynamic variables

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -55,8 +55,8 @@ class EventsPage extends Component {
 
     const requestBody = {
       query: `
-          mutation {
-              createEvent(eventInput: {title: "${title}", description: "${description}", price: ${price} , date:"${date}"}) {
+          mutation CreateEvent($title: String!, $description: String!, $price: Float!, $date: String!) {
+              createEvent(eventInput: {title: $title, description: $description, price: $price , date: $date}) {
                   _id
                   title
                   description
@@ -65,6 +65,12 @@ class EventsPage extends Component {
                 }
           }
       `,
+      variables: {
+        title: title,
+        description: description,
+        price: price,
+        date: date,
+      },
     };
 
     const token = this.context.token;
@@ -176,14 +182,17 @@ class EventsPage extends Component {
 
     const requestBody = {
       query: `
-          mutation {
-              bookEvent(eventId: "${this.state.selectedEvent._id}") {
+          mutation BookEvent($id: ID!){
+              bookEvent(eventId: $id) {
                   _id
                   createdAt
                   updatedAt
                 }
           }
       `,
+      variables: {
+        id: this.state.selectedEvent._id,
+      },
     };
 
     fetch('http://localhost:8000/graphql', {
